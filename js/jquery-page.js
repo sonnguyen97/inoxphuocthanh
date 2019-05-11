967d942728000e688460a1b358793454048d28a1
sonnguyen97 <nguyencongson163@gmail.com>
2019-05-11 08:42:51 +0700

    update cart

--- js/jquery-page.js
+++ js/jquery-page.js
@@ -2,60 +2,62 @@ function page(pagenumber) {
     if (pagenumber != null) {
         switch (pagenumber) {
             case 1:
-                $(function () { $("#body_pages").load("index.html") });
+                $(function() { $("#body_pages").load("index.html") });
                 break;
             case 2:
-                $(function () { $("#body_pages").load("shop.html") });
+                $(function() { $("#body_pages").load("shop.html") });
                 break;
             case 3:
-                $(function () { $("#body_pages").load("blog-single.html") });
+                $(function() { $("#body_pages").load("blog-single.html") });
                 break;
             case 4:
-                $(function () { $("#body_pages").load("about-us.html") });
+                $(function() { $("#body_pages").load("about-us.html") });
                 break;
             case 5:
-                $(function () {
+                $(function() {
                     $("#body_pages").load("contact.html")
                 });
                 break;
             case 6:
-                $(function () { $("#body_pages").load("single-product.html") });
+                $(function() { $("#body_pages").load("single-product.html") });
+                break;
+            case 7:
+                $(function() { $("#body_pages").load("cart.html") });
                 break;
             default:
                 break;
         }
     }
 }
+
 function pagecontact(pagenumber) {
 
     if (pagenumber != null) {
         window.location.href = "layout.html";
-        
+
         switch (pagenumber) {
             case 1:
-                $(function () { $("#body_pages").load("index.html") });
+                $(function() { $("#body_pages").load("index.html") });
                 break;
             case 2:
-                $(function () { $("#body_pages").load("shop.html") });
+                $(function() { $("#body_pages").load("shop.html") });
                 break;
             case 3:
-                $(function () { $("#body_pages").load("blog-single.html") });
+                $(function() { $("#body_pages").load("blog-single.html") });
                 break;
             case 4:
-                $(function () { $("#body_pages").load("about-us.html") });
+                $(function() { $("#body_pages").load("about-us.html") });
                 break;
             case 5:
-                $(function () {
+                $(function() {
                     $("#body_pages").load("contact.html")
                 });
                 break;
             case 6:
-                $(function () { $("#body_pages").load("single-product.html") });
+                $(function() { $("#body_pages").load("single-product.html") });
                 break;
             default:
                 break;
         }
     }
 }
-
-
